dfd21622ec65f783b99350b1cdf6d20cffd367d4
Mazay98 <zavolod@yandex.ru>
2019-12-22 15:43:33 +0300

    init

---
+++ index.php
@@ -0,0 +1,19 @@
+<!doctype html>
+<html lang="ru">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="assets/styles/style.css">
+    <script
+            src="https://code.jquery.com/jquery-3.4.1.min.js"
+            integrity="sha256-CSXorXvZcTkaix6Yvo6HppcZGetbYMGWSFlBw8HfCJo="
+            crossorigin="anonymous"></script>
+    <title>Chat</title>
+</head>
+<body>
+
+<script src="assets/scripts/script.js"></script>
+</body>
+</html>
