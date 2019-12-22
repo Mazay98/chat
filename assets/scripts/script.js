0fe4df9c87a6318dbb9a93a7bbec6c19e075d491
Mazay98 <zavolod@yandex.ru>
2019-12-22 16:51:47 +0300

    front-path

---
+++ assets/scripts/script.js
@@ -0,0 +1,32 @@
+document.addEventListener("DOMContentLoaded", function(){
+    let socket = new WebSocket("ws://chat:8090/server.php");
+    let blockMessage = "chat__result";
+
+    socket.onerror = function(error) {
+        message("<div class='block-error'>"+ "Ошибка при присоединени" +"</div>");
+    };
+
+    socket.onopen = function () {
+        message( "<div class='block-success'>Соединение установлено.</div>");
+    }
+
+    socket.onclose = function(event) {
+        message("<div class='block-error'>"+"Соединение закрыто"+"</div>");
+    };
+
+    socket.onmessage = function(event) {
+        let data = JSON.parse(event.data);
+        message("<div class='block-info'>"+ data.type + "-" +data.message + "</div>");
+    };
+
+
+
+});
+
+function message(text, block = 'chat__result') {
+    try {
+        document.getElementById(block).innerHTML = text;
+    } catch (e) {
+        alert(e);
+    }
+}
--- index.php
+++ index.php
@@ -6,14 +6,20 @@
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <link rel="stylesheet" href="assets/styles/style.css">
-    <script
-            src="https://code.jquery.com/jquery-3.4.1.min.js"
-            integrity="sha256-CSXorXvZcTkaix6Yvo6HppcZGetbYMGWSFlBw8HfCJo="
-            crossorigin="anonymous"></script>
+<!--    <script-->
+<!--            src="https://code.jquery.com/jquery-3.4.1.min.js"-->
+<!--            integrity="sha256-CSXorXvZcTkaix6Yvo6HppcZGetbYMGWSFlBw8HfCJo="-->
+<!--            crossorigin="anonymous"></script>-->
     <title>Chat</title>
 </head>
 <body>
+    <div class="container">
+        <form class="chat" action="">
+            <div class="chat__result" id="chat__result">
 
+            </div>
+        </form>
+    </div>
 <script src="assets/scripts/script.js"></script>
 </body>
 </html>
